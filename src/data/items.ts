36bf5840159c7071b5eac2caec3b59286a037dd0
Daniel Vitor <danielvitorzero@gmail.com>
2023-04-10 22:04:48 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item"
+
+export const items: Item[] = [
+    { date: new Date(2023, 2, 15), category: 'food', title: 'Burger King', value: 48.90 },
+    { date: new Date(2023, 2, 16), category: 'rent', title: 'Aluguel', value: 1900 },
+    { date: new Date(2023, 2, 19), category: 'food', title: 'McDonalds', value: 35.60 },
+    { date: new Date(2023, 3, 10), category: 'salary', title: 'Salário empresa', value: 5000 },
+];
